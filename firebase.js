18e66b19f0a004b7e5e252f671bb14d9fdd1699e
Anuj Trivedi <103488296+aasn0119@users.noreply.github.com>
2026-06-15 06:26:08 +0000

    added firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFPScPyDCVfUtV-avI6qsCMkelpCbDglQ",
+    authDomain: "cgl-mocks-tracker.firebaseapp.com",
+    projectId: "cgl-mocks-tracker",
+    storageBucket: "cgl-mocks-tracker.firebasestorage.app",
+    messagingSenderId: "422134753390",
+    appId: "1:422134753390:web:bebdedf065fc41f8ba5e7f",
+    measurementId: "G-B5EJ2KXH03"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
